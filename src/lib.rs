758d61d9f15bc2b771d14e2c0e5582e3d0f17b7e
bluss <bluss@users.noreply.github.com>
2017-10-08 13:57:12 +0200

    DOC: Add crate example

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,35 @@
 
+//! Unchecked index through the regular index syntax.
+//!
+//! Using a wrapper type that requires an `unsafe` block to create.
+//!
+//! # Example
+//!
+//! ```rust
+//!
+//! use unchecked_index::unchecked_index;
+//!
+//! /// unsafe because: trusts the permutation to be correct
+//! unsafe fn apply_permutation<T>(perm: &mut [usize], v: &mut [T]) {
+//!     debug_assert_eq!(perm.len(), v.len());
+//!     
+//!     // use unchecked (in reality, debug-checked) indexing throughout
+//!     let mut perm = unchecked_index(perm);
+//!     
+//!     for i in 0..perm.len() {
+//!         let mut current = i;
+//!         while i != perm[current] {
+//!             let next = perm[current];
+//!             // move element from next to current
+//!             v.swap(next, current);
+//!             perm[current] = current;
+//!             current = next;
+//!         }
+//!         perm[current] = current;
+//!     }
+//! }
+//! ```
+
 // Because pain and SliceExt
 //#![no_std]
 
